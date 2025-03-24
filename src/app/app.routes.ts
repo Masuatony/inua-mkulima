95ba11d5ce9d83e2997eca7ef296e6aa2d6d0937
Tonysoft12 <127377528+Masuatony@users.noreply.github.com>
2025-03-24 14:43:15 +0300

    finished products definition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./auth/login/login.component";
-import {ProductListComponent} from "./features/components/product-list/product-list.component";
 import {MainLayoutComponent} from "./lay/main-layout/main-layout.component";
+import {ProductComponent} from "./features/components/product/product.component";
 
 export const routes: Routes = [
   {
     path: 'login', component: LoginComponent
   },
   {
-    path: 'products', component: MainLayoutComponent,
+    path: '', component: MainLayoutComponent,
     children: [
-      {path: '', component: ProductListComponent}
+      {path: 'products', component: ProductComponent}
     ]
   }
 ];
--- src/app/features/components/product/product.component.ts
+++ src/app/features/components/product/product.component.ts
@@ -1,12 +1,92 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {ProductService} from "../services/product.service";
+import {CommonModule, NgFor} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatCheckbox} from "@angular/material/checkbox";
+
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    CommonModule,
+    NgFor,
+    MatIcon,
+    MatIconButton,
+    MatTableModule,
+    MatCheckbox
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  items: any ;
+  selectedProduct:any;
+  totalPrice: any;
+  selectedProducts: any;
+
+
+  displayedColumns: string[] = ['select', 'title','price'];
+  dataSource = new MatTableDataSource<any>();
+  selection = new SelectionModel<any>(true, []);
+
+
+
+  constructor(private service : ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.service.fetchProducts().subscribe((data) => {
+      this.items = data.products;
+      this.dataSource = new  MatTableDataSource<any>(data.products);
+    });
+  }
+
+
+  editProduct(item: any) {
+    this.selectedProduct = item;
+    const index = this.selectedProducts.findIndex((product: any) => product.name === item.name);
+    if (index === -1) {
+      this.selectedProducts.push(item);
+    } else {
+      this.selectedProducts.splice(index, 1);
+    }
+
+    // this.totalPrice = this.selectedProducts.reduce((sum, product) => sum + product.price, 0);
+  }
+
+
+
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
 
 }
